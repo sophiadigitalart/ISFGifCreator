6bd1b3be31d874b16e63159dee2533809ca6ffa2
Bruce <bruce@batchass.fr>
2020-01-16 14:08:37 +0100

    new ofxisf for of11

--- clone_addons.cmd
+++ clone_addons.cmd
@@ -2,6 +2,6 @@ cd ../../../addons/
 
 git clone https://github.com/videodromm/ofxGifEncoder.git
 
-git clone https://github.com/brucelane/ofxISF.git
-
+rem git clone https://github.com/brucelane/ofxISF.git
+git clone -b of11 https://github.com/iansouth/ofxISF
 echo "Done cloning addons! :)"
